140eba465ba26ef524375481257eea42dc98aa20
Nguyễn Quốc Anh <anhnquoc.dev@gmail.com>
2022-08-24 11:43:19 +0700

    add pexel api

--- Pixzor/Program.cs
+++ Pixzor/Program.cs
@@ -1,3 +1,10 @@
+global using Pixzor.Resources;
+global using System.Net.Http.Json;
+global using System.Net.Http.Headers;
+global using Pixzor.Models;
+global using Pixzor.Services;
+global using System.Text.Json.Serialization;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,5 +18,19 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+builder.Services.AddScoped<IPhotoService, PhotoService>();
+
+/* Pexel API */
+builder.Services.AddHttpClient("PexelPhoto", client =>
+{
+    client.BaseAddress = new Uri(Constants.BASE_URL);
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(Constants.API_KEY);
+});
+builder.Services.AddHttpClient("PexelVideo", client =>
+{
+    client.BaseAddress = new Uri(Constants.VIDEO_BASE_URL);
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(Constants.API_KEY);
+});
 
 await builder.Build().RunAsync();
---
+++ Pixzor/Resources/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Pixzor.Resources
+{
+    public static class Constants
+    {
+        public const string BASE_URL = "https://api.pexels.com/v1/";
+        public const string VIDEO_BASE_URL = "https://api.pexels.com/videos/";
+        public const string API_KEY = "563492ad6f91700001000001f14055bafe18434ead839ea5c8095240";
+    }
+}
