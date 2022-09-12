f55723cecc4ff0e304a0193a7d23eea8ae41728e
Nguyễn Quốc Anh <anhnquoc.dev@gmail.com>
2022-09-12 17:25:53 +0700

    half done detail photo

--- Pixzor/Models/Photo.cs
+++ Pixzor/Models/Photo.cs
@@ -2,16 +2,27 @@
 {
     public class Photo
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("width")]
         public int Width { get; set; }
+        [JsonPropertyName("height")]
         public int Height { get; set; }
-        public string Url { get; set; }
-        public string Photographer { get; set; }
-        public string PhotographerUrl { get; set; }
+        [JsonPropertyName("url")]
+        public string Url { get; set; } = string.Empty;
+        [JsonPropertyName("photographer")]
+        public string Photographer { get; set; } = string.Empty;
+        [JsonPropertyName("photographer_url")]
+        public string PhotographerUrl { get; set; } = string.Empty;
+        [JsonPropertyName("photographer_id")]
         public int PhotographerId { get; set; }
-        public string AvgColor { get; set; }
+        [JsonPropertyName("avg_color")]
+        public string AvgColor { get; set; } = string.Empty;
+        [JsonPropertyName("src")]
         public Source Src { get; set; }
+        [JsonPropertyName("liked")]
         public bool Liked { get; set; }
-        public string Alt { get; set; }
+        [JsonPropertyName("alt")]
+        public string Alt { get; set; } = string.Empty;
     }
 }
